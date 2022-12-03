7776c51df941f1df4475e1e90accbd96063d2da7
webstar0411 <webstar0411@gmail.com>
2022-12-04 00:41:16 +0800

    enhance seo

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -10,7 +10,12 @@ const Layout: NextPage<Props> = ({ children }) => {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Facebook clone, Facebook v2" />
+        <meta
+          name="keywords"
+          content="Facebook clone, Facebook v2, Next.js, Tailwind CSS clone"
+        />
+        <meta name="author" content="Erys Mozo | @ErysCode7" />
         <title>Facebook</title>
       </Head>
       <div>{children}</div>
