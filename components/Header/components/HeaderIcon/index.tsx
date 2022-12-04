20e6fd5f6eb71942ebb6f102d743554bb9329023
webstar0411 <webstar0411@gmail.com>
2022-12-04 14:23:04 +0800

    fix

--- components/Header/components/HeaderIcon/index.tsx
+++ components/Header/components/HeaderIcon/index.tsx
@@ -4,12 +4,17 @@ type HeroIcon = (props: React.ComponentProps<"svg">) => JSX.Element;
 
 type Props = {
   Icon: HeroIcon;
+  active?: boolean;
 };
 
-const HeaderIcon: NextPage<Props> = ({ Icon }) => {
+const HeaderIcon: NextPage<Props> = ({ Icon, active }) => {
   return (
     <div className="cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 flex items-center rounded-xl active:border-b-2 active:border-blue-500 group">
-      <Icon className="h-5  text-center sm:h-7 mx-auto group-hover:text-blue-500" />
+      <Icon
+        className={`h-5 text-gray-500 text-center sm:h-7 mx-auto group-hover:text-blue-500 ${
+          active && "text-blue-500"
+        }`}
+      />
     </div>
   );
 };
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,59 +1,83 @@
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from "@heroicons/react/solid";
 import type { NextPage } from "next";
-import { SearchIcon, FlagIcon, PlayIcon, ShoppingCartIcon } from "@heroicons/react/outline";
-import { BellIcon, ChatIcon, ChevronDownIcon, HomeIcon, UserGroupIcon, ViewGridIcon } from "@heroicons/react/solid"
 import Image from "next/image";
 import HeaderIcon from "./components/HeaderIcon/";
 
 const Header: NextPage = () => {
-
-    const icons = [
-        {
-            icon: HomeIcon,
-        },
-        {
-            icon: FlagIcon,
-        },
-        {
-            icon: PlayIcon,
-        },
-        {
-            icon: ShoppingCartIcon,
-        },
-        {
-            icon: UserGroupIcon,
-        },
-    ]
+  const icons = [
+    {
+      icon: HomeIcon,
+      active: true,
+    },
+    {
+      icon: FlagIcon,
+    },
+    {
+      icon: PlayIcon,
+    },
+    {
+      icon: ShoppingCartIcon,
+    },
+    {
+      icon: UserGroupIcon,
+    },
+  ];
 
   return (
     <header className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
-        {/* LEFT */}
+      {/* LEFT */}
       <div className="flex items-center">
         <Image
-            src="/images/facebook-logo.png"
-            width={40}
-            height={40}
-            alt="Facebook logo"
+          src="/images/facebook-logo.png"
+          width={40}
+          height={40}
+          alt="Facebook logo"
         />
         <div className="flex items-center rounded-full bg-gray-100 p-2">
-            <SearchIcon className="h-6 text-gray-600" />
-            <input type="text" className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" placeholder="Search Facebook" />
+          <SearchIcon className="h-6 text-gray-600" />
+          <input
+            type="text"
+            className="hidden md:flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            placeholder="Search Facebook"
+          />
         </div>
       </div>
 
-    {/* MID */}
-    <div className="flex justify-center flex-grow">
+      {/* MID */}
+      <div className="flex justify-center flex-grow">
         <div className="flex space-x-6 md:space-x-2">
-            {icons.map(icon => (
-                <HeaderIcon Icon={icon.icon} />
-            ))}
+          {icons.map((icon) => (
+            <HeaderIcon Icon={icon.icon} active={icon.active} />
+          ))}
         </div>
-    </div>
+      </div>
 
+      {/* Right */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* Profile pic */}
+        <Image />
 
-            {/* Right */}
+        <p className="font-semibold pr-3 whitespace-nowrap">Erys Mozo</p>
+        <ViewGridIcon />
+        <ChatIcon />
+        <BellIcon />
+        <ChevronDownIcon />
+      </div>
     </header>
   );
 };
 
 export default Header;
-
