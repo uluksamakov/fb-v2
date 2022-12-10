b19ef52eea6286cc7959c99b94cd54fd57d59091
webstar0411 <webstar0411@gmail.com>
2022-12-10 14:52:49 +0800

    configure firebase

---
+++ config/firebase.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const storage = getStorage();
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+};
+
+// Initialize Firebase
+const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
+const database = getFirestore(app);
+
+export { app, database };
