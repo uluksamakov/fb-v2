aa06482ba8d5a9d61a2f938c908eb2b1c956dc4d
webstar0411 <webstar0411@gmail.com>
2022-12-22 22:12:02 +0800

    fix

--- components/Feed/index.tsx
+++ components/Feed/index.tsx
@@ -4,7 +4,7 @@ import { InputBox, Posts, Stories } from "./components";
 const Feed: NextPage = () => {
   return (
     <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto scrollbar-hide">
-      <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
+      <div className="mx-auto w-full sm:max-w-md md:max-w-lg lg:max-w-2xl">
         {/* STORIES */}
         <Stories />
         {/* INPUT BOX */}
